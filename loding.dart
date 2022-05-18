1a69210b0d00225655b51beae7e91aa75f9b7e65
Jihoon9809 <95200335+Jihoon9809@users.noreply.github.com>
2022-05-18 23:22:25 +0900

    Update loding.dart

--- loding.dart
+++ loding.dart
@@ -21,15 +21,17 @@ class _LoadingState extends State<Loading>{
   @override
   void initState(){
     super.initState();
-    getLocation();
+    getLocation(); //앱 실행시 위도 경도 찾기
   }
 
+  // getLocation 함수를 통하여 위도 경도를 다운받는다.
   void getLocation() async{
     MyLocation myLocation = MyLocation();
-    await myLocation.getMyCurrentLocation();
+    await myLocation.getMyCurrentLocation(); // 위도와 경도값이 리턴되는 것을 기다려야 하므로 await 적용.
     latitude3 = myLocation.latitude2;
     longitude3 = myLocation.longitude2;
 
+    //데이터 가져오기.
     Network network = Network('https://api.openweathermap.org/data/2.5/weather?lat=${latitude3}&lon=${longitude3}&appid=$apikey&units=metric');
 
     var weatherData = await network.getJsonData();
@@ -38,12 +40,6 @@ class _LoadingState extends State<Loading>{
       return WeatherScreen(parseWeatherData: weatherData,);
     }));
   }
-
-  // void fetchDate() async{
-  //
-
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context){
