22e8dc07908c3cf516a46a7e83ff1c7f58a2bf38
Jihoon9809 <95200335+Jihoon9809@users.noreply.github.com>
2022-05-18 23:23:05 +0900

    Update network.dart

--- network.dart
+++ network.dart
@@ -4,13 +4,14 @@ import 'dart:convert';
 class Network {
   final String url;
 
-  Network(this.url);
+  Network(this.url); //인스턴스가 생성될때 생성자를 통해 url을 받음
 
+  //json의 데이터 타입이 다양하므로 dynamic설정
   Future<dynamic> getJsonData() async {
     http.Response response = await http.get(Uri.parse(url)); //문법 변경됨
     if (response.statusCode == 200) {
-      String jsonData = response.body;
-      var parsingData = jsonDecode(jsonData);
+      String jsonData = response.body; //다운받은 데이터를 jsonData로 저장.
+      var parsingData = jsonDecode(jsonData); // 데이터 파싱
       return parsingData;
     }
   }
