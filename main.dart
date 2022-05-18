4a0fc630d6f4ab3634b696811f25eddf90aebd14
Jihoon9809 <95200335+Jihoon9809@users.noreply.github.com>
2022-05-18 23:22:05 +0900

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news/screens/loding.dart';
+import 'package:news/screens/news_app.dart';
 
 
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Loading(),
+      home: NewsApp(),
     );
   }
 }
