4d548a095ffffcf24a502ab9980f4be630aa8217
Jihoon9809 <95200335+Jihoon9809@users.noreply.github.com>
2022-05-19 10:57:39 +0900

    Update home.dart

--- home.dart
+++ home.dart
@@ -1,62 +1,55 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget{
-  Home({Key? key}) : super(key:key);
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home>{
+class _HomeState extends State<Home> {
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     final home_logo = Text( //어플 로고이미지로 변경
-      "로고 위치",
+      "News",
       style: TextStyle(
           color: Colors.black),
     );
+    final Leading_button = IconButton( onPressed: () {print("검색버튼");},icon: Icon(Icons.search), color: Colors.grey,);
 
     final current_location = Text(
-      "현재 위치",
+      "현재 주소 \n 기온",
       style: TextStyle( // 위도 및 경로를 활용하여 현위치 이름으로 변경.
           height: 2.2,
-          fontSize: 22,
+          fontSize: 13,
           color: Colors.black),
-      textAlign: TextAlign.center,
     );
 
-    final current_icon = IconButton(
-        onPressed: (){
-          print("위치 버튼 눌림.");
-        }, icon: Icon(Icons.location_pin),
-        color: Colors.black);
 
-    Widget _bodyWidget(){
-      return ListView.separated(itemBuilder: (
-
-          BuildContext _context, int
-          index){
+    Widget _bodyWidget() {
+      return ListView.separated(itemBuilder: (BuildContext _context, int
+      index) {
         return Container(
-          child: Row(
+          child: Column(
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(10)), // 이미지 모서리 둥글게 설정.
-                child: Image.asset("assets/images/ara-${index+1}.jpg",
-                  width:100,
-                  height:100,
-                ),
+              Image.asset("assets/images/ara-${index + 1}.jpg",
+                width: 500,
+                height: 200,
               ),
+              Container(
+                  child: Column(
+                    children: [
+                      Text("기사글 들어갈 공간")
+                    ],
+                  ))
             ],
           ),
         );
       },
         itemCount: 10
         ,
-        separatorBuilder: (
-
-            BuildContext _context, int
-            index){
+        separatorBuilder: (BuildContext _context, int
+        index) {
           return Container(height: 1, color: Colors.black.withOpacity(0.4));
         },
       );
@@ -65,14 +58,19 @@ class _HomeState extends State<Home>{
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        title : home_logo,
-        actions: <Widget>[
-          current_location,
-          current_icon,
-        ],
-      ),
-      body: _bodyWidget(),
+          backgroundColor: Colors.white,
+          title: home_logo,
+          centerTitle: true,
+          leading: Leading_button,
+          actions: <Widget>[ IconButton(onPressed: (){print("문의사항 이동");}, icon: Icon(Icons.info),color: Colors.grey,),
+            IconButton(onPressed: (){print("세팅 이동");}, icon: Icon(Icons.settings),color: Colors.grey,),
+      ],
+    ),body
+    :
+    _bodyWidget
+    (
+    )
+    ,
     );
   }
 }
