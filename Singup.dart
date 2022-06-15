e6f43d6f8ef5e5ab459ae5320b16049587726e1e
Jihoon9809 <95200335+Jihoon9809@users.noreply.github.com>
2022-06-15 18:43:24 +0900

    Update Singup.dart

--- Singup.dart
+++ Singup.dart
@@ -2,6 +2,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:news/screens/bottombar.dart';
+import 'package:news/screens/login.dart';
 import 'package:news/screens/myuser.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:news/screens/profile.dart';
@@ -49,7 +51,7 @@ class _SingupPageState extends State<SingupPage> {
         .doc(user.uid)
         .set(myuser.toMap());
 
-    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => ProfileScreen()));
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => bottom()));
   }
 
 
@@ -64,6 +66,85 @@ class _SingupPageState extends State<SingupPage> {
 
     final Info_button = IconButton(onPressed: (){print("문의사항 이동");}, icon: Icon(Icons.info),color: Colors.grey,);
 
+    final SignUp_screen = Container(
+      child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 30),
+              Container(
+                child:  Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("News",
+                        style: TextStyle(fontSize:70,
+                            fontWeight: FontWeight.bold)),
+                    Text("Sign into your account",
+                        style: TextStyle(fontSize:20,
+                            color: Colors.grey[500])),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50),
+              Container(
+                width: 350,
+                child:
+                TextFormField(
+                  controller: _emailController,
+                  onSaved: (value){
+                    _emailController.text = value!;
+                  },
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.person_outline),
+                    hintText: 'ID',
+                    contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              Container(
+                width: 350,
+                child:
+                TextFormField(
+                    controller: _passwrodController,
+                    onSaved: (value){
+                      _passwrodController.text = value!;
+                    },
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.lock_outline),
+                      hintText: 'password',
+                      contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                      ),
+                    )
+                ),),
+              SizedBox(height: 30),
+              ButtonBar(
+                alignment : MainAxisAlignment.spaceAround,
+                buttonPadding: EdgeInsets.all(10),
+                children: [
+                  TextButton(
+                      onPressed: (){
+                        Navigator.pop(context);
+                      },
+                      child: Text('Do you have Id?')),
+                  ElevatedButton(
+                      onPressed: () async{
+                        signUp(_emailController.text,_passwrodController.text);
+                      },
+                      child: Text('submit')),
+                ],
+              ),
+            ],
+          )
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -72,73 +153,16 @@ class _SingupPageState extends State<SingupPage> {
         leading: Leading_button,
         actions: <Widget>[ Info_button,
       ],),
-      body: Container(
-        child: Form(
-          key: _formKey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 100),
-                Text('Sing up',
-                    style: TextStyle( fontSize: 30, fontWeight: FontWeight.bold)),
-                SizedBox(height: 30),
-                Container(
-                  width: 350,
-                  child:
-                  TextFormField(
-                    controller: _emailController,
-                      onSaved: (value){
-                        _emailController.text = value!;
-                      },
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.person_outline),
-                        hintText: 'ID',
-                        contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                        border: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                        ),
-                      ),
-                  ),
-                ),
-                SizedBox(height: 30),
-                Container(
-                  width: 350,
-                  child:
-                  TextFormField(
-                    controller: _passwrodController,
-                      onSaved: (value){
-                        _passwrodController.text = value!;
-                      },
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.lock_outline),
-                        hintText: 'password',
-                        contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                        border: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                        ),
-                      )
-                  ),),
-                SizedBox(height: 30),
-                ButtonBar(
-                  alignment : MainAxisAlignment.spaceAround,
-                  buttonPadding: EdgeInsets.all(10),
-                  children: [
-                    TextButton(
-                        onPressed: (){
-                          Navigator.pop(context);
-                        },
-                        child: Text('Do you have Id?')),
-                    ElevatedButton(
-                        onPressed: () async{
-                          signUp(_emailController.text,_passwrodController.text);
-                        },
-                        child: Text('submit')),
-                  ],
-                ),
-              ],
-            )
-        ),
-      ),
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder:(context, snapshot) {
+          if(snapshot.hasData){
+            return ProfileScreen();
+          }else{
+            return SignUp_screen;
+          }
+        },
+      )
     );
   }
 }
