30f399a3a8dd218725e0164c5953d3e70e43af40
Jihoon9809 <95200335+Jihoon9809@users.noreply.github.com>
2022-06-16 16:20:31 +0900

    Update login.dart

--- login.dart
+++ login.dart
@@ -22,7 +22,7 @@ class _LoginPageState extends State<LoginPage> {
   int _sucess = 1;
   String _userEmail = "";
 
-  void signin(String email, String password) async {
+  void login(String email, String password) async {
     if (_formKey.currentState!.validate()) {
       try {
         await _auth
@@ -38,7 +38,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final home_logo = Text( //어플 로고이미지로 변경
-      "News",
+      "Log In!",
       style: TextStyle(
           color: Colors.black),
     );
@@ -50,22 +50,22 @@ class _LoginPageState extends State<LoginPage> {
       child: Form(
         key: _formKey,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            SizedBox(height: 50),
+            SizedBox(height: 30),
             Container(
               child:  Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("News",
-                      style: TextStyle(fontSize:70,
+                  Text("Welcome\n  Back!",
+                      style: TextStyle(fontSize:35,
                           fontWeight: FontWeight.bold)),
-                  Text("Log into your account",
+                  Text("Hello Welcome back!!",
                       style: TextStyle(fontSize:20,
                           color: Colors.grey[500])),
                 ],
               ),
             ),
-            SizedBox(height: 50),
+            SizedBox(height: 35),
             Container(
               width: 350,
               child:
@@ -76,12 +76,12 @@ class _LoginPageState extends State<LoginPage> {
                     hintText: 'ID',
                     contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
                     border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
                     ),
                   )
               ),
             ),
-            SizedBox(height: 30),
+            SizedBox(height: 20),
             Container(
               width: 350,
               child:
@@ -93,7 +93,7 @@ class _LoginPageState extends State<LoginPage> {
                     hintText: 'password',
                     contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
                     border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
                     ),
                   )
               ),),
@@ -101,63 +101,51 @@ class _LoginPageState extends State<LoginPage> {
                 alignment: Alignment.center,
                 child: Text(_sucess == 1 ? " ": (_sucess == 2 ? '로그인 성공':'로그인 실패'),
                 )),
-            SizedBox(height: 30),
+            SizedBox(height: 10),
             ButtonBar(
               alignment : MainAxisAlignment.spaceAround,
               buttonPadding: EdgeInsets.all(10),
               children: [
                 TextButton(
-                    onPressed: (){                        },
-                    child: Text('forget password?',)),
-                ElevatedButton(
-                    onPressed: ()async{
-                      signin(_emailcontroller.text,_passwordcontroller.text);
-                    },
-                    child: Text('Login')),
-              ],
-            ),
-            Text('Sing up', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            ButtonBar(
-                alignment : MainAxisAlignment.start,
-                children: [
-                  TextButton(
-                      onPressed: (){},
-                      child: Text("             Don't have an account?")),
-                ]),
-            ButtonBar(
-              alignment : MainAxisAlignment.spaceAround,
-              buttonPadding: EdgeInsets.all(10),
-              children: [
-                TextButton(
-                    onPressed: (){},
-                    child: Text('')),
-                ElevatedButton(
                     onPressed: (){
                       Navigator.push(context,MaterialPageRoute(builder: (context) => SingupPage()),);
                     },
                     child: Text('Go Sing up')),
-              ],),
+                ElevatedButton(
+                    onPressed: ()async{
+                      login(_emailcontroller.text,_passwordcontroller.text);
+                    },
+                    child: Text('Login')),
+              ],
+            ),
           ],),
       ),);
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: home_logo,
-        centerTitle: true,
-        leading: Leading_button,
-        actions: <Widget>[ Info_button,
-        ],),
-      body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder:(context, snapshot) {
-          if(snapshot.hasData){
-            return ProfileScreen();
-          }else{
-            return logIn_screen;
-          }
-        },
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/login.png'), fit: BoxFit.cover)
       ),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            title: home_logo,
+            centerTitle: true,
+            leading: Leading_button,
+            actions: <Widget>[ Info_button,
+            ],),
+          body: StreamBuilder<User?>(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder:(context, snapshot) {
+                if(snapshot.hasData){
+                  return ProfileScreen();
+                }else{
+                  return logIn_screen;
+                }
+              },
+            ),
+          ),
     );
   }
 }
