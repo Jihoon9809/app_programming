cdd917081a176772f5c4446b150a123f4d19fb8e
Jihoon9809 <95200335+Jihoon9809@users.noreply.github.com>
2022-06-16 16:21:28 +0900

    Update profile.dart

--- profile.dart
+++ profile.dart
@@ -28,14 +28,32 @@ class _ProfileScreenState extends State<ProfileScreen> {
       });
     });
   }
-
-
+  Widget user_image(){
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: SizedBox(
+            width: 100,
+            height: 100,
+              child: Image.asset('assets/images/user.png',fit: BoxFit.cover,),
+       ),
+      )
+    ]
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+    backgroundColor: Colors.transparent,
+      body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            SizedBox(height: 50),
+            Container(
+              child: user_image(),),
+            SizedBox(height: 30),
             Container(
               child: Text(
                 '이메일: ${loggedInUser.email}',
@@ -46,7 +64,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-            )
+            ),
+            SizedBox(height: 50),
+                    ElevatedButton.icon(onPressed: ()=> FirebaseAuth.instance.signOut(),
+                    icon: Icon(Icons.arrow_back,size:32),
+                    label: Text(
+                      'Sign Out',
+                      style: TextStyle(fontSize: 24),
+                    )),
           ],
         ),
       ),
